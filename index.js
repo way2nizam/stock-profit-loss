3e25cb2baf2f4a79c6a33c3da80cbe62ea0d3e79
Nizam <nizamy710@gmail.com>
2021-09-16 14:32:06 +0530

    fix: code refactor

--- index.js
+++ index.js
@@ -1,51 +1,56 @@
-const initialPrice = document.querySelector("#initial-price");
-const stocksCount = document.querySelector("#stocks-count");
-const currentPrice = document.querySelector("#current-price");
-const checkButton = document.querySelector("#check-btn");
-const showOutput = document.querySelector("#show-output");
+const initialPrice = document.querySelector('#initial-price');
+const stocksCount = document.querySelector('#stocks-count');
+const currentPrice = document.querySelector('#current-price');
+const checkButton = document.querySelector('#check-btn');
+const showOutput = document.querySelector('#show-output');
 
 function outputMessage(message) {
-  showOutput.innerHTML = message;
+  showOutput.innerText = 'Result: ' + message;
 }
 
 function calculateProfit() {
+  document.querySelector('body').style.backgroundColor = '#7FE5F0';
   const profit = (currentPrice.value - initialPrice.value) * stocksCount.value;
   const profitPercentage =
     (profit * 100) / (initialPrice.value * stocksCount.value);
 
   outputMessage(
-    "Profit is " +
+    'Profit value is ₹ ' +
       profit +
-      " and Profit percentage is " +
+      ' and Profit percentage is ' +
       profitPercentage +
-      "%."
+      '%.'
   );
 }
 
 function calculateLoss() {
+  document.querySelector('body').style.backgroundColor = '#FF2E2E';
+
   const loss = (initialPrice.value - currentPrice.value) * stocksCount.value;
   const lossPercentage =
     (loss * 100) / (initialPrice.value * stocksCount.value);
-
   outputMessage(
-    "Loss is " + loss + " and Loss percentage is " + lossPercentage + "%."
+    'Loss is ' + loss + ' and Loss percentage is ' + lossPercentage + '%.'
   );
 }
 
 function clickHandler() {
-  if (
-    currentPrice.value * stocksCount.value >
-    initialPrice.value * stocksCount.value
-  ) {
-    calculateProfit();
-  } else if (
-    initialPrice.value * stocksCount.value >
-    currentPrice.value * stocksCount.value
-  ) {
-    calculateLoss();
+  if (stocksCount.value < 0) {
   } else {
-    outputMessage("No Pain No Gain");
+    if (
+      currentPrice.value * stocksCount.value >
+      initialPrice.value * stocksCount.value
+    ) {
+      calculateProfit();
+    } else if (
+      initialPrice.value * stocksCount.value >
+      currentPrice.value * stocksCount.value
+    ) {
+      calculateLoss();
+    } else {
+      outputMessage('No Pain No Gain');
+    }
   }
 }
 
-checkButton.addEventListener("click", clickHandler);
+checkButton.addEventListener('click', clickHandler);
